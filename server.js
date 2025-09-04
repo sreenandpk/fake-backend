dad47dde53f8f66f491a0bd086f2d4b3766f424f
Davidd132 <sreenandpk3@gmail.com>
2025-09-05 00:08:40 +0530

    Your commit message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import jsonServer from "json-server";
+const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -9,5 +9,5 @@ server.use(router);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`✅ JSON Server running on port ${port}`);
+  console.log(`✅ JSON Server is running on port ${port}`);
 });
